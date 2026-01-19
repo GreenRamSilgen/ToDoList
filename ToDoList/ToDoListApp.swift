4c0b9cf2a4db73ee25513cbef66e5ce4bdade2ab
GreenRamSilgen <kiranshrs@yahoo.com>
2026-01-19 07:33:31 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Kiran Shrestha on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
